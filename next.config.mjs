f6ff69b8fac2b3a073c9a646b566cd10d9ddf51f
Maksym Korostelov <flying1519@gmail.com>
2025-04-17 15:03:35 +0300

    next config modified

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/todo-list' : '',
-  assetPrefix: isProd ? '/todo-list/' : '',
+  basePath: isProd ? '/' : '',
+  assetPrefix: isProd ? '/' : '',
 };
 
 export default nextConfig;
